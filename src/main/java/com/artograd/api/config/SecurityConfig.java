72e290b315acdfe2bd8e5056a9bc9a1549def590
Vitalii Kolesov <Vitalii_Kolesov@epam.com>
2024-02-02 12:44:34 +0400

    feat: update SecurityConfig
    Part of EPMGCIPART-4

--- src/main/java/com/artograd/api/config/SecurityConfig.java
+++ src/main/java/com/artograd/api/config/SecurityConfig.java
@@ -1,15 +1,13 @@
 package com.artograd.api.config;
 
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
@@ -17,24 +15,17 @@ import java.util.Arrays;
 import java.util.List;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(requests -> requests
+                        .requestMatchers("/hello").permitAll()
                         .anyRequest().authenticated())
-                .oauth2ResourceServer(oauth2ResourceServer ->
-                        oauth2ResourceServer.jwt(Customizer.withDefaults())
-                )
-                .logout(logout -> logout
-                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                        .permitAll()
-                        .logoutSuccessHandler((httpServletRequest, httpServletResponse, authentication) ->
-                                httpServletResponse.setStatus(HttpServletResponse.SC_OK))
-                        .deleteCookies("JSESSIONID")
-                        .invalidateHttpSession(true))
-                .csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).disable())
+                .oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer.jwt(Customizer.withDefaults()))
+                .csrf(Customizer.withDefaults())
                 .cors(SecurityConfig::configureCors)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
@@ -44,7 +35,7 @@ public class SecurityConfig {
     private static void configureCors(CorsConfigurer<HttpSecurity> cors) {
         CorsConfiguration config = new CorsConfiguration();
         config.addAllowedOriginPattern("*");
-        config.setAllowedMethods(Arrays.asList("GET","POST", "DELETE", "PUT", "PATCH", "HEAD"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE", "PUT", "PATCH", "HEAD"));
         config.setAllowedHeaders(List.of("Origin", "Accept", "X-Requested-With", "Content-Type",
                 "Access-Control-Request-Method", "Access-Control-Request-Headers"));
         config.setExposedHeaders(List.of("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
