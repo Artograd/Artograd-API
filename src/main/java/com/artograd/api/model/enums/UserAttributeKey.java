0501f4a193e54b5d2fe240eb6859fc7ed33cde8c
Maksim-Totski <Maksim.Totski@gmail.com>
2024-03-29 09:34:57 +0100

    Urgent fix for missing attributes

--- src/main/java/com/artograd/api/model/enums/UserAttributeKey.java
+++ src/main/java/com/artograd/api/model/enums/UserAttributeKey.java
@@ -9,6 +9,7 @@ public enum UserAttributeKey {
     CUSTOM_INSTAGRAM("custom:instagram"),
     CUSTOM_LINKEDIN("custom:linkedin"),
     CUSTOM_LOCATION("custom:location"),
+    WEBSITE("website"),
     CUSTOM_ORGANIZATION("custom:organization"),
     CUSTOM_JOBTITLE("custom:jobtitle"),
     GIVEN_NAME("given_name"),
@@ -18,6 +19,7 @@ public enum UserAttributeKey {
     CUSTOM_LANG_ISO2("custom:lang_iso2"),
     COGNITO_GROUPS("cognito:groups"),
     EMAIL("email"),
+    EMAIL_VERIFIED("email_verified"),
     SHOW_EMAIL("custom:show_email"),
     BANK_ACCOUNT("custom:bank_account"),
     BANK_BENEFICIARY("custom:bank_benefit_bank"),
@@ -25,7 +27,8 @@ public enum UserAttributeKey {
     BANK_IBAN("custom:bank_iban"),
     BANK_SWIFT("custom:bank_swift"),
     BANK_USE_DEFAULT("custom:bank_use_default"),
-    PHONE_NUMBER("phone_number");
+    PHONE_NUMBER("phone_number"),
+	SUB("sub");
 
     private final String attributeKey;
 
