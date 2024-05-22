539e1e23ba952981b02eccd5edbb4096145d1efa
Vitalii Kolesov <50338025+VitBK@users.noreply.github.com>
2024-05-22 22:39:01 +0400

    Add PreSignUpLambdaHandler to check if email is already in use. Adds aws-lambda-java-events dependency
    * feat: adds PreSignUpLambdaHandler to check if email is already in use. Adds aws-lambda-java-events dependency
    
    Resolves EPMGCIPART-182
    
    * chore: fix indentation in pom.xml
    
    * refactor: apply google format
    
    * refactor: apply google format 2

---
+++ src/main/java/com/artograd/api/PreSignUpLambdaHandler.java
@@ -0,0 +1,28 @@
+package com.artograd.api;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.CognitoUserPoolPreSignUpEvent;
+import software.amazon.awssdk.services.cognitoidentityprovider.CognitoIdentityProviderClient;
+import software.amazon.awssdk.services.cognitoidentityprovider.model.ListUsersRequest;
+import software.amazon.awssdk.services.cognitoidentityprovider.model.ListUsersResponse;
+
+public class PreSignUpLambdaHandler
+    implements RequestHandler<CognitoUserPoolPreSignUpEvent, CognitoUserPoolPreSignUpEvent> {
+
+  @Override
+  public CognitoUserPoolPreSignUpEvent handleRequest(
+      CognitoUserPoolPreSignUpEvent event, Context context) {
+    try (CognitoIdentityProviderClient cognitoClient =
+        CognitoIdentityProviderClient.builder().build()) {
+      String userPoolId = event.getUserPoolId();
+      ListUsersRequest usersRequest =
+          ListUsersRequest.builder().userPoolId(userPoolId).filter(event.getUserName()).build();
+      ListUsersResponse response = cognitoClient.listUsers(usersRequest);
+      if (!response.users().isEmpty()) {
+        throw new RuntimeException("User with this email is already registered");
+      }
+      return event;
+    }
+  }
+}
