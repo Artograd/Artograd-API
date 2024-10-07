bfffcf6d585406054614793a616561dd9c23329b
Andrii Borozdykh <andrii_borozdykh@epam.com>
2024-10-07 10:09:47 -0400

    [EPMGCIPART-403] Update User Profile Data for ArtObject, Proposal, and Tender

---
+++ src/main/java/com/artograd/api/TenderStatusUpdateLambdaHandler.java
@@ -0,0 +1,39 @@
+package com.artograd.api;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.ScheduledEvent;
+import com.artograd.api.model.Tender;
+import com.artograd.api.model.enums.TenderStatus;
+import com.artograd.api.repositories.TenderRepository;
+import java.util.Date;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TenderStatusUpdateLambdaHandler implements RequestHandler<ScheduledEvent, String> {
+
+  private static final ApplicationContext context;
+
+  static {
+    context = new AnnotationConfigApplicationContext(Application.class);
+  }
+
+  @Autowired private TenderRepository tenderRepository;
+
+  @Override
+  public String handleRequest(ScheduledEvent event, Context context) {
+    Date today = new Date();
+    List<Tender> tenders =
+        tenderRepository.findBySubmissionStartAndStatus(today, TenderStatus.PUBLISHED.toString());
+
+    for (Tender tender : tenders) {
+      System.out.println(tender);
+    }
+
+    return "Tenders updated successfully";
+  }
+}
--- src/main/java/com/artograd/api/repositories/TenderRepository.java
+++ src/main/java/com/artograd/api/repositories/TenderRepository.java
@@ -1,6 +1,7 @@
 package com.artograd.api.repositories;
 
 import com.artograd.api.model.Tender;
+import java.util.Date;
 import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
@@ -9,4 +10,7 @@ public interface TenderRepository extends MongoRepository<Tender, String> {
 
   @Query("{ '$or': [ { 'ownerId': ?0 }, { 'proposals.ownerId': ?0 } ] }")
   List<Tender> findByOwnerIdOrProposalOwnerId(String ownerId);
+
+  @Query("{ 'submissionStart': ?0, 'status': ?1 }")
+  List<Tender> findBySubmissionStartAndStatus(Date submissionStart, String status);
 }
